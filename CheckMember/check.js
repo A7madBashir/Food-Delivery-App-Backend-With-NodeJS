6ca43a838912d5e16f4122a9f913cfc1b90d350e
TCC-Team <ahmadsy8122@gmail.com>
2021-02-15 00:20:56 +0200

    make it better

---
+++ CheckMember/check.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const sql = require("mssql");
+
+app.use(express.json());
+//connect to DB
+const config = {
+  user: "DB_A6F580_FoodDelivery01_admin",
+  password: "123456789FD",
+  server: "SQL5102.site4now.net",
+  database: "DB_A6F580_FoodDelivery01",
+options: {
+    "enableArithAbort": true,
+    },  
+};
+
+sql.connect(config, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("connection succ");
+});
+app.get('/check',(req,res)=>{
+  console.log("TestWorld");
+})
+
+app.get("/checkmember", async (req, res) => {
+  const result = await sql.query`select * from customer`;
+  //select [me_id],[description],[price],[image],[name] from [DB_A6F580_FoodDelivery01].[dbo].[meal]
+  res.status(200).json([...result.recordset]);
+	
+});
+
+app.listen(3000, function () {
+  console.log("the server started");
+});
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const sql = require("mssql");
+
+app.use(express.json());
+//connect to DB
+const config = {
+  user: "DB_A6F580_FoodDelivery01_admin",
+  password: "123456789FD",
+  server: "SQL5102.site4now.net",
+  database: "DB_A6F580_FoodDelivery01",
+options: {
+    "enableArithAbort": true,
+    },  
+};
+
+sql.connect(config, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("connection succ");
+});
+
+app.get("/", (req, res) => {
+  res.send("It's All Good!");
+});
+app.get("/order", (req, res) => {
+  //read from DB
+  res.json({ name: "rssss" });
+});
+
+app.get("/dish", async (req, res) => {
+  const result = await sql.query`SELECT TOP (1000) [id]
+      ,[UserName]
+  FROM [DB_A6F580_FoodDelivery01].[dbo].[Users]`;
+  res.status(200).json([...result.recordset]);
+	
+});
+
+app.listen(3000, function () {
+  console.log("the server started");
+});
