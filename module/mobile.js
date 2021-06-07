0e5f7a567ec66993a162a6de740b99b86ef924c6
A7mad Bashir <bashir8122@gmail.com>
2021-06-07 18:51:55 +0300

    up

--- module/mobile.js
+++ module/mobile.js
@@ -51,9 +51,9 @@ Router
   })
 //Meals By Resturant ID 
 Router
-  .route('/Meal/ByResturantId/:rest_id')
+  .route('/Resturant/byid/:rest_id')
   .get(async(req,res)=>{
-    const result=await sql.query(`select meal.m_id ,meal.m_name, meal.image, meal.price ,meal.m_count ,meal.Description from have,meal where rest_id=${req.params.rest_id}`);
+    const result=await sql.query(`select * from resturant where rest_id=1=${req.params.rest_id}`);
     res.status(200).json([...result.recordset]);
   })
 Router
@@ -64,7 +64,7 @@ Router
   })
 
 Router
-  .route('/Resturant/byid/:rest_id')
+  .route('/Meal/ByResturantId/:rest_id')
   .get(async (req,res)=>{
     const result=await sql.query(`select Distinct meal.m_id,meal.[image],meal.[price],meal.m_count,meal.m_name,meal.[Description] from resturant,meal,have where have.rest_id=${req.params.rest_id} and meal.m_id=have.m_id`);
     res.status(200).json([...result.recordset]);
