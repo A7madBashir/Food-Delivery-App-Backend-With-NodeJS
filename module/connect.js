f3976566542a8c6ace576ada106138c200ae2a1e
A7mad Bashir <eagle81222@gmail.com>
2021-06-18 22:45:02 +0300

    update Database source

--- module/connect.js
+++ module/connect.js
@@ -1,10 +1,10 @@
 const sql=require("mssql");
 //connect to DB
 const config = {
-    user: "db_a72f14_fooddelivery02_admin",
+    user: "db_a763da_fooddelivery03_admin",
     password: "123456789FD",
-    server: "SQL5104.site4now.net",
-    database: "db_a72f14_fooddelivery02",
+    server: "SQL5053.site4now.net",
+    database: "db_a763da_fooddelivery03",
     options: {  
       enableArithAbort: true,
     },
