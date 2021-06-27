84c895318c16b87e05b94fc9dd8fb21def2eb82e
A7mad Bashir <eagle81222@gmail.com>
2021-06-28 00:12:43 +0300

    a

--- index.js
+++ index.js
@@ -101,11 +101,12 @@ io.on("connection", function (socket) {
   //After send data to the database it's should get the last order that added
   //so here we can join room that customer joined by order id from get-delivery event
   socket.on("order-room", (room) => {
-    const count = io.clients;                    
+    const count = io.to(`${room}`).clients;                    
     console.log("order room and members count:", room, "\t", count.length);
     if (count.length < 2) {           
+      io.to(`${room}`).clients.length++;
       socket.join(`${room}`);
-      console.log("Joining Delivery The Room With customer:",io.clients.length);
+      console.log("Joining Delivery The Room With customer:",io.to(`${room}`).clients.length);
       socket.emit("canOrder", true);
     } else {
       socket.emit("canOrder", false);
