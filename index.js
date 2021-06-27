0321a1d57d5a501233a0f272a713cca29cc7799b
A7mad Bashir <eagle81222@gmail.com>
2021-06-27 23:50:10 +0300

    upsRW

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ io.on("connection", function (socket) {
   //so here we can join room that customer joined by order id from get-delivery event
   socket.on("order-room", (room) => {
     // const count = io.to(`${room}`).clients;        
-    const count= socket.to(`${room}`).clients;    
+    const count= io.sockets.socket.to(`${room}`).clients;    
     // console.log("order room and members count:", room, "\t", count);
     console.log("order room and members count:", room, "\t", count.length);
     if (count.length < 2) {           
