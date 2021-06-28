eeaba4debe27f2396a9481b018759b4ddc7efe18
A7mad Bashir <eagle81222@gmail.com>
2021-06-28 15:04:51 +0300

    a

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ io.on("connection", function (socket) {
   //so here we can join room that customer joined by order id from get-delivery event
   socket.on("order-room", (room) => {
     // const count = io.to(`${room}`).clients;/* +1?? */     
-    const count=io.engine.to(`${room}`).clientsCount;
+    const count=io.sockets.clients(`${room}`);
     console.log("order room and members count:", room, "\t", count);
     if (count < 2) {                 
       socket.join(`${room}`);
